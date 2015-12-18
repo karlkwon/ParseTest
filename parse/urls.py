a3357b80814a4b617b24bb8c3ffd990a03afe447
LonelyWolf Lee <lonelywolf.lee@gmail.com>
2015-12-18 07:03:16 +0000

    link with real page and embed chart js file

--- parse/urls.py
+++ parse/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.index.as_view(), name='index'),
     url(r'^chart', views.ChartView.as_view(), name='chart'),
 ]
